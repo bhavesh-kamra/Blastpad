375bb58fb2a1cfdae6eaa6437c14cf653a6f3112
bhavesh-kamra <kamrabhavesh08@gmail.com>
2023-05-24 10:23:25 +0530

    changes

--- src/components/Staking.js
+++ src/components/Staking.js
@@ -1,12 +1,14 @@
 import React from "react";
 import StakeBlastpad from "./StakeBlastpad";
 import StakeTokens from "./StakeTokens";
+import StakeBlastpadBonus from "./StakeBlastpadBonus";
 
 const Staking = () => {
   return (
     <>
       <StakeBlastpad />
       <StakeTokens />
+      <StakeBlastpadBonus />
     </>
   );
 };
