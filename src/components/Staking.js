dc78ec1daf1b6f3f3e07d3920ce2a59b211c607c
kapilverma <vermakp39@gmail.com>
2023-05-24 10:21:36 +0530

    ok

--- src/components/Staking.js
+++ src/components/Staking.js
@@ -1,12 +1,14 @@
 import React from "react";
 import StakeBlastpad from "./StakeBlastpad";
 import StakeTokens from "./StakeTokens";
+import Footer from "./Footer";
 
 const Staking = () => {
   return (
     <>
       <StakeBlastpad />
       <StakeTokens />
+      
     </>
   );
 };
