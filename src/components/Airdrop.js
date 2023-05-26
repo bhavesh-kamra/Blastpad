ae775bf673af0e645198144b0463cd3033971249
kapilverma <vermakp39@gmail.com>
2023-05-26 17:25:35 +0530

    ok

--- src/components/Airdrop.js
+++ src/components/Airdrop.js
@@ -2,12 +2,16 @@ import React from "react";
 import ChooseTheAirDrop from "./ChooseTheAirDrop";
 import MyNav from "./MyNav";
 import ClaimAirdrop from "./ClaimAirdrop";
+import Footer from "./Footer";
 
 const Airdrop = () => {
   return (
     <>
       <MyNav />
       <ClaimAirdrop />
+      <Airdrop />
+      <ChooseTheAirDrop />
+      <Footer />
     </>
   );
 };
