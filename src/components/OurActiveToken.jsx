e79d562fcf867090e03d81d6fc8465493fe5b63d
kapilverma <vermakp39@gmail.com>
2023-05-26 11:13:31 +0530

    om

--- src/components/OurActiveToken.jsx
+++ src/components/OurActiveToken.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import MetalDao from "../assets/images/png/metal.png";
 import present12 from "../assets/images/png/Parsent12.png";
+import rocket from "../assets/images/png/rocket.png";
 const OurActiveToken = () => {
   return (
     <>
       <section>
         <div className=" container max-w-[1176px] mx-auto px-3">
-          <div className=" grid grid-cols-3">
-            <div className="">
+          <div className=" grid grid-cols-3 gap-6">
+            <div className=" Stake_Your_Card p-4">
               <div className=" flex ">
                 <img
                   src={MetalDao}
@@ -27,7 +28,7 @@ const OurActiveToken = () => {
                 <button className=" px-3 py-2 bg-[#12174C] font-Raleway font-normal text-xsm text-white rounded-[22px]">
                   Total raise 69k
                 </button>
-                <button className=" mx-3 px-3 py-2 bg-[#12174C] font-Raleway font-normal text-xsm text-white rounded-[22px]">
+                <button className=" mx-2 px-3 py-2 bg-[#12174C] font-Raleway font-normal text-xsm text-white rounded-[22px]">
                   value 3.2M
                 </button>
                 <button className=" px-3 py-2 bg-[#12174C] font-Raleway font-normal text-xsm text-white rounded-[22px]">
@@ -39,12 +40,25 @@ const OurActiveToken = () => {
                 <p className=" font-Raleway font-normal text-xsm text-white ">
                   0/87,074 MECHA
                 </p>
-                <p className=" font-Raleway font-normal text-xsm text-white me-14">
+                <p className=" font-Raleway font-normal text-xsm text-white me-4">
                   10 BUSD
                 </p>
               </div>
-              <div className="">
-                <button>Active</button>
+              <div className="flex items-center justify-between mt-8">
+                <button className="slider_btn font-Raleway font-semibold text-white text-sm px-12 py-3">
+                  Active
+                </button>
+                <div className=" flex me-0">
+                  <a href="#">
+                    <img src={rocket} alt="rocate" />
+                  </a>
+                  <a
+                    href="#"
+                    className=" font-Inter font-medium text-sm text-white ps-2 "
+                  >
+                    BlastPad.io
+                  </a>
+                </div>
               </div>
             </div>
           </div>
